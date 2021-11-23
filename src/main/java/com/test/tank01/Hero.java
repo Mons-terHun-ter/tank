c3a872f10cbaa5aa2380c8b709c5d583674e2ebe
libo.a <cjcslhp:012138ml>
2021-11-23 19:58:12 +0800

    优化图片加载

--- src/main/java/com/test/tank01/Hero.java
+++ src/main/java/com/test/tank01/Hero.java
@@ -1,15 +1,25 @@
 package com.test.tank01;
 
+import com.test.commonenum.Direction;
 import lombok.Data;
+
+import javax.swing.*;
+import java.text.MessageFormat;
 
 @Data
 public class Hero extends Tank {
 
     private Integer speed = 10;
+    private String imagePrefix = "E1tank{0}.png";
 
     public Hero(Integer x,Integer y) {
         this.setX(x);
         this.setY(y);
+        this.upImage = new ImageIcon(ClassLoader.getSystemResource(MessageFormat.format(this.getImagePrefix(), Direction.UP.getCode())).getPath()).getImage();
+        this.downImage = new ImageIcon(ClassLoader.getSystemResource(MessageFormat.format(this.getImagePrefix(), Direction.DOWN.getCode())).getPath()).getImage();
+        this.leftImage = new ImageIcon(ClassLoader.getSystemResource(MessageFormat.format(this.getImagePrefix(), Direction.LEFT.getCode())).getPath()).getImage();
+        this.rightImage = new ImageIcon(ClassLoader.getSystemResource(MessageFormat.format(this.getImagePrefix(), Direction.RIGHT.getCode())).getPath()).getImage();
+        this.setCurrentImage(upImage);
     }
 
 }
--- src/main/java/com/test/tank01/Tank.java
+++ src/main/java/com/test/tank01/Tank.java
@@ -1,23 +1,24 @@
 package com.test.tank01;
 
 import com.test.commonenum.Direction;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import javax.swing.*;
 import java.awt.*;
-import java.text.MessageFormat;
 
 @Data
 public abstract class Tank {
-    private String imagePrefix = "E1tank{0}.png";
+    private String imagePrefix;
     private Integer x = 10;
     private Integer y = 10;
     private Direction direction = Direction.UP;
     private Integer speed = 1;
+    Image upImage;
+    Image downImage;
+    Image leftImage;
+    Image rightImage;
+    Image currentImage = upImage;
 
     public void draw(Graphics g) {
-        g.drawImage(new ImageIcon(ClassLoader.getSystemResource(MessageFormat.format(this.getImagePrefix(), direction.getCode())).getPath()).getImage(), this.getX(), this.getY(), null);
+        g.drawImage(this.getCurrentImage(), this.getX(), this.getY(), null);
     }
 
     public void move(Direction direction) {
@@ -25,15 +26,19 @@ public abstract class Tank {
         switch (direction) {
             case UP:
                 y = y - this.getSpeed();
+                currentImage = upImage;
                 break;
             case DOWN:
                 y = y + this.getSpeed();
+                currentImage = downImage;
                 break;
             case LEFT:
                 x = x - this.getSpeed();
+                currentImage = leftImage;
                 break;
             case RIGHT:
                 x = x + this.getSpeed();
+                currentImage = rightImage;
                 break;
             default:
         }
